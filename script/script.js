fd8f580588a5d66d63ed020e1e02ad680f0d5072
Irina Chirkunova <irina.chirkunova86@gmail.com>
2019-07-02 00:23:51 +0200

    Add index.html, scrit.js and scripts

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+alert('Peace and love!');
+
+console.log('Hello my friends');
+
+console.log('Carpe diem');
+
+
+
